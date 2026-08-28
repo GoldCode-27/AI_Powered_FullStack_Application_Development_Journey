96f6909160abbbd29a3ac3465d021a3e21d30bb2
GoldCode-27 <workinehwor@gmail.com>
2026-08-28 20:42:13 +0300

    feat adding Header component

--- Introduction_to_AI_powered_application/001_Netflix_Clone/NETFLIX/src/App.jsx
+++ Introduction_to_AI_powered_application/001_Netflix_Clone/NETFLIX/src/App.jsx
@@ -1,12 +1,12 @@
 
-import './App.css'
+// import './App.css'
 import Banner from'./Components/Banner/Banner.jsx'
+import Header from'./Components/Header/Header.jsx'
 
 function App() {
-
-
   return (
     <>
+    <Header />
     <Banner />
     </>
   )
