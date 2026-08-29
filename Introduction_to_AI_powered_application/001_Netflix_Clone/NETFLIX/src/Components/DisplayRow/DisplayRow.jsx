74d791050d9321a5f65f5b246271e423426a4b40
GoldCode-27 <workinehwor@gmail.com>
2026-08-29 21:19:30 +0300

    docs: updating the existing documentation(readme)

--- Introduction_to_AI_powered_application/001_Netflix_Clone/NETFLIX/src/Components/DisplayRow/DisplayRow.jsx
+++ Introduction_to_AI_powered_application/001_Netflix_Clone/NETFLIX/src/Components/DisplayRow/DisplayRow.jsx
@@ -2,11 +2,31 @@ import React from 'react'
 import Styles from './DisplayRow.module.css'
 import SlideShow from '../SlideShow/SlideShow'
 import { movies } from '../../Data/Data'
-
+import {movieInstance} from'../../Utility/MovieInstance'
+import requests from'../../Utility/requestURL'
 function DisplayRow() {
+
+const [movie, setMovies] = useState({
+  trending:[],
+  netFlixOriginals:[],
+  topRated:[],
+  action:[],
+  commedy:[],
+  horror:[],
+  romance:[],
+  documentaties:[]
+})
+
+const fetchMovies= async ()=>{
+  try{
+
+  }catch(error){
+    
+  }
+}
+
   return (
     <div className={Styles.mainWraper}>
-      {/* movie={movies} የነበረው ወደ movies={movies} ተቀይሯል */}
       <SlideShow title="Movies Suggestions" movies={movies} />
       <SlideShow title="Popular on Netflix" movies={movies} />
       <SlideShow title="Trending Now" movies={movies} />
