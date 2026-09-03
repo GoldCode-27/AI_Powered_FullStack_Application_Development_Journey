bf38979fc199c84f804b8c15eded92a625d4fe44
GoldCode-27 <workinehwor@gmail.com>
2026-09-03 13:33:29 +0300

    fix: updating the node_modules on the frontend part

---
+++ 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/server.js
@@ -0,0 +1,33 @@
+import express from'express'
+import db from'./db/dbConfig.js'
+import mainRouter from'./src/api/main.route.js'
+import errorHandler from'./src/middleware/error.handler.js'
+        const app = express();
+
+        // Middleware to parse JSON requests
+        app.use(express.json());
+        app.use('/api', mainRouter);
+
+        app.use(errorHandler); // Use the error handler middleware
+
+
+ const startServer = async () => {
+    try {
+        // Test the database connection
+        const connection = await db.getConnection();
+        console.log('Connected to database');
+        
+        //releasing the connection back to the pool
+        connection.release();
+
+        // Start the server
+        const PORT = 3888;
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error('Failed to connect to the database:', error.message);
+    }
+}
+
+startServer();
