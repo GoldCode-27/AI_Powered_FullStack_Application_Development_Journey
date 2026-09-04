a3d61dadf6757191d376bdd8e3806f92faeb3eec
GoldCode-27 <workinehwor@gmail.com>
2026-09-04 10:14:56 +0300

    fix: improve error handling and logging in error handler middleware

--- 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/middleware/error.handler.js
+++ 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/middleware/error.handler.js
@@ -1,12 +1,11 @@
-
 //function to handle errors
 export const errorHandler = (err, req, res, next) => {
-   console.log("error:", err.message);
-    return
-     res.status(500).json({
-        status: false,
-        message: err.message || 'something went wrong try again later',
-     });
-    }
+  console.error("error:", err.message);
+
+  return res.status(err.status || 500).json({
+    status: false,
+    message: err.message || "Something went wrong. Try again later.",
+  });
+};
 
 export default errorHandler;
