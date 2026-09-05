5cbeb7974f8a4b80b1b64f3262e969fcf6632f13
GoldCode-27 <workinehwor@gmail.com>
2026-09-05 15:09:33 +0300

    refactor: standardize import statements and add authRouter to mainRouter

--- 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/api/main.route.js
+++ 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/api/main.route.js
@@ -1,12 +1,14 @@
 //importing express router
-import express from 'express';
-import chatRouter from './chat/chat.route.js';
+import express from "express";
+import chatRouter from "./chat/chat.route.js";
+import authRouter from "./auth/auth.route.js";
 
 //importing the express router
 const mainRouter = express.Router();
 
 // Mount the chatRouter on the mainRouter
-mainRouter.use('/chat', chatRouter);
+mainRouter.use("/chat", chatRouter);
+mainRouter.use("/auth", authRouter);
 
 // Endpoint to create a new chat conversation
 export default mainRouter;
