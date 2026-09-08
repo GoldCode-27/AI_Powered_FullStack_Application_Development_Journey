894f98efa6dc932dde2f0eb4f06957691be02121
GoldCode-27 <workinehwor@gmail.com>
2026-09-09 01:42:21 +0300

    feat: add validation middleware for signup and login routes

--- 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/api/auth/auth.route.js
+++ 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/api/auth/auth.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { loginController, signupController } from "./auth.controller.js";
+import {
+  validateLogin,
+  validateSignup,
+} from "../../middleware/authValidator.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/signup", signupController);
-authRouter.post("/login", loginController);
+authRouter.post("/signup", validateSignup, signupController);
+authRouter.post("/login", validateLogin, loginController);
 
 export default authRouter;
