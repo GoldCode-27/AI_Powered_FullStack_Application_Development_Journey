0cf5bcaecaf001a9500bc154b9db8c9ebe55f442
GoldCode-27 <workinehwor@gmail.com>
2026-09-16 03:06:25 +0300

    refactor: restructure server initialization to use async function

--- 03_AI_Powered_Evangadi_Forum/backend/server.js
+++ 03_AI_Powered_Evangadi_Forum/backend/server.js
@@ -1,9 +1,14 @@
 import express from'express'
-const app=express();
+const app = express();
 
 
-app.listen(process.env.PORT, (err)=>{
+const StartServer = async()=>{
+    
+  app.listen(process.env.PORT, (err)=>{
     if(err)
         console.log(err.message);
     console.log(`server is running on port http://localhost${process.env.PORT}`)
 })
+}
+
+StartServer();
