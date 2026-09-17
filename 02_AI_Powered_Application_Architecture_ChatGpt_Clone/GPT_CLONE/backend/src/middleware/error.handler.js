3e64bffd1a38f471da143e18f5258aa21de6aa2f
GoldCode-27 <workinehwor@gmail.com>
2026-09-17 16:49:34 +0300

    feat:adding errorHandling middleware for centeralizing error handling mechanism

--- 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/middleware/error.handler.js
+++ 02_AI_Powered_Application_Architecture_ChatGpt_Clone/GPT_CLONE/backend/src/middleware/error.handler.js
@@ -2,6 +2,7 @@
 export const errorHandler = (err, req, res, next) => {
   console.error("error:", err.message);
 
+
   return res.status(err.status || 500).json({
     status: false,
     message: err.message || "Something went wrong. Try again later.",
