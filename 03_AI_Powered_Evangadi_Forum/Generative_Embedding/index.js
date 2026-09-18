f3c1d3592a9aec39bbd6e817a43cc9f665c20d09
GoldCode-27 <workinehwor@gmail.com>
2026-09-18 19:08:27 +0300

    fix:applying try catch to handle the exact error and by making it asyncrnous function for waiting the app until the model responds

--- 03_AI_Powered_Evangadi_Forum/Generative_Embedding/index.js
+++ 03_AI_Powered_Evangadi_Forum/Generative_Embedding/index.js
@@ -3,17 +3,25 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const GEMINI_API_KEYy = process.env.GEMINI_API_KEY;
 
 const ai = new GoogleGenAI({
-    apiKey:GEMINI_API_KEY
+    apiKey:GEMINI_API_KEYy
 });
 
 async function main() {
+    try{
+
+
+   
   const interaction = await ai.models.embedContent({
     model: "gemini-embedding-001",
     contents: "How does AI work?",
   });
   console.log(interaction.embeddings);
+
+ }catch(error){
+    console.log("error when fetching from the model",error)
+ }
 }
-await main();
+main();
